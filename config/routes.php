ed6b5f77bd443fa91e146d3e55d3185e057b2df3
shulha <shulhad@gmail.com>
2017-03-10 11:32:01 +0200

    Add config/routes

---
+++ config/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    "root" => [
+        "pattern" => "/",
+        "method" => "GET",
+        "action" => "Mystore\\IndexController@index"
+    ],
+    "product_list" =>
+        [
+            "pattern" => "/product",
+            "method" => "GET",
+            "action" => "Mystore\\ProductController@getAllGoods"
+        ],
+    "single_product" => [
+        "pattern" => "/product/{id}",
+        "method" => "GET",
+        "variables" => [
+            "id" => "\d+"
+        ],
+        "action" => "Mystore\\ProductController@getProduct"
+    ]
+];
